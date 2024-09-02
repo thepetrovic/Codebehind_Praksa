fc21600b64436db6bf4087a3f5bf46f76b8f7f17
stefan.petrovic1 <p.stefan44035@gmail.com>
2024-09-02 22:53:45 +0200

    Dodao sam zadatak

---
+++ Olimpijske_igre/Olimpijske_igre/UcitajJSON.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Olimpijske_igre
+{
+    internal  class UcitajJSON
+    {
+        public string groupLocation = Directory.GetCurrentDirectory() + "\\json\\";
+        
+        public  T LoadJSON<T>(string file)
+        {
+            string jsonString = File.ReadAllText(groupLocation + file);
+            return JsonSerializer.Deserialize<T>(jsonString);
+        }
+       
+        
+
+    }
+}
