c3a0566bac17572f161ccf6557113887d1d3dfdb
stefan.petrovic1 <p.stefan44035@gmail.com>
2024-09-02 23:10:53 +0200

    Dodat zadatak

---
+++ Olimpijske_igre/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Olimpijske_igre
+{
+    public class Program()
+    {
+        static void Main(string[] args)
+        {
+            Simulacija simulacija= new Simulacija();
+            simulacija.CreateGroups();
+
+            simulacija.PlayFirstRound();
+            simulacija.PlaySecondRound();
+            simulacija.PlayThirdRound();
+            simulacija.RankGroup();
+            simulacija.WriteGroup();
+
+            simulacija.RankTeamsOneToEight();
+            simulacija.AddPots();
+            simulacija.WritePots();
+            simulacija.EliminationPhase();
+            simulacija.PlayEliminationPhase();
+            simulacija.PlaySemiFinalsMatch();
+            simulacija.FinalMatchs();
+            
+
+        }
+    }
+
+}
---
+++ Olimpijske_igre/TeamJSON.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olimpijske_igre
+{
+    internal class TeamJSON
+    {
+        public string Team { get; set; }
+        public string ISOCode { get; set; }
+        public int FIBARanking { get; set; }
+        public TeamJSON(string Team, string iSOCode, int fIBARanking)
+        {
+            this.Team = Team;
+            this.ISOCode = iSOCode;
+            this.FIBARanking = fIBARanking;
+        }
+        public override string ToString()
+        {
+            return "Team: "+ Team + "("+ISOCode+") - FIBARanking: "+FIBARanking;
+        }
+    }
+}
